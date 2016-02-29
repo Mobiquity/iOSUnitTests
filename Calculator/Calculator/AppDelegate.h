0c4412de40be5cdebd48d2c3ec20049c0147c036
Brendan Carey <careybrendant@gmail.com>
2016-02-29 08:58:12 -0500

    Create project.

---
+++ Calculator/Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Brendan Carey on 2/29/16.
+//  Copyright © 2016 Mobiquity, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calculator/Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Brendan Carey on 2/29/16.
+//  Copyright © 2016 Mobiquity, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
