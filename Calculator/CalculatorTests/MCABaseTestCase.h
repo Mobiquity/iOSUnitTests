969781cbf7a5d90ce999fbd7754a709ab2a14c7a
Brendan Carey <careybrendant@gmail.com>
2016-02-29 09:06:12 -0500

    Add base test case.

---
+++ Calculator/CalculatorTests/MCABaseTestCase.h
@@ -0,0 +1,13 @@
+//
+//  MCABaseTestCase.h
+//  Calculator
+//
+//  Created by Brendan Carey on 2/29/16.
+//  Copyright © 2016 Mobiquity, Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MCABaseTestCase : XCTestCase
+
+@end
---
+++ Calculator/CalculatorTests/MCABaseTestCase.m
@@ -0,0 +1,13 @@
+//
+//  MCABaseTestCase.m
+//  Calculator
+//
+//  Created by Brendan Carey on 2/29/16.
+//  Copyright © 2016 Mobiquity, Inc. All rights reserved.
+//
+
+#import "MCABaseTestCase.h"
+
+@implementation MCABaseTestCase
+
+@end
