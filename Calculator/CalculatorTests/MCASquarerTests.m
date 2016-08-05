21e441e60f337009ffb5d05b3921420cc741e595
Chris Nielubowicz <cnielubowicz@mobiquityinc.com>
2016-08-04 23:44:45 -0400

    Adds more failing unit tests -- first tests of input variance

--- Calculator/CalculatorTests/MCASquarerTests.m
+++ Calculator/CalculatorTests/MCASquarerTests.m
@@ -30,4 +30,25 @@
     XCTAssertNotNil(squarer, @"MCASquarer should be initialized, not nil.");
 }
 
+- (void)testInitializationWithValidInput {
+    MCASquarer *squarer = [[MCASquarer alloc] initWithString:@"3.14"];
+    XCTAssertNotNil(squarer, @"MCASquarer should be initialized, not nil.");
+    XCTAssertNotNil(squarer.operand, @"Operand property should be initialized, not nil.");
+}
+
+- (void)testInitializationWithInvalidInput {
+    MCASquarer *squarer = [[MCASquarer alloc] initWithString:@"abcdefg.hijk"];
+    XCTAssertNil(squarer, @"MCASquarer should be not initialized");
+}
+
+- (void)testInitializationWithNilInput {
+    MCASquarer *squarer = [[MCASquarer alloc] initWithString:nil];
+    XCTAssertNil(squarer, @"MCASquarer should be not initialized");
+}
+
+- (void)testInitializationWithGarbageInput {
+    MCASquarer *squarer = [[MCASquarer alloc] initWithString:((NSString *)@{@"dictionary" : @"value"})];
+    XCTAssertNil(squarer, @"MCASquarer should be not initialized");
+}
+
 @end
