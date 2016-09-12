d7fd129bf7b269ee364901f48bd3c76a0720ad84
Kelly McEachern <kmceachern@mobiquityinc.com>
2016-09-12 15:37:39 -0400

    Added additional percent encoding to MCAWolframAlphaQuery to match what Wolfram Alpha API expects. Created additional testWolframURLEscaping test. Modified UI constraints so that result cell is not partially hidden when the phone goes into Landscape mode.

--- Calculator/CalculatorTests/MCAWolframAlphaQueryTests.m
+++ Calculator/CalculatorTests/MCAWolframAlphaQueryTests.m
@@ -28,6 +28,16 @@
     XCTAssertNotNil(query.queryURL);
 }
 
+- (void)testWolframURLEscaping {
+    NSString *queryString = @"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-_.~!#$&'()*+,/:;=?@[]";
+    NSString *expected = @"http://api.wolframalpha.com/v2/query?input=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-_.~!%23%24%26'()*%2B%2C%2F%3A%3B%3D%3F%40%5B%5D&appid=7455XJ-6VH386Y4Y4&format=plaintext";
+    
+    MCAWolframAlphaQuery *query = [[MCAWolframAlphaQuery alloc] initWithQueryText:queryString];
+    NSString *result = [query.queryURL absoluteString];
+    
+    XCTAssertTrue([expected isEqualToString:result]);
+}
+
 - (void)testURLEncoding {
     NSString *eulerQuery = @"euler's law";
     MCAWolframAlphaQuery *query = [[MCAWolframAlphaQuery alloc] initWithQueryText:eulerQuery];
